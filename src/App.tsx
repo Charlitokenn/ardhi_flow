e33d9890f8431206d7bd0082cbf528af7d09d2db
Charles Nkonoki <charlitokenn@gmail.com>
2026-08-07 02:11:03 +0300

    added clerk auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import './App.css'
 import {ThemeProvider} from "@/components/theme-provider.tsx";
 import {ModeToggle} from "@/components/mode-toggle.tsx";
+import {Show, SignInButton, SignUpButton, UserButton} from "@clerk/react";
 
 function App() {
 
   return (
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <header>
+              <Show when="signed-out">
+                  <SignInButton />
+                  <SignUpButton />
+              </Show>
+              <Show when="signed-in">
+                  <UserButton />
+              </Show>
+          </header>
         <div className="text-red-700">Hello World</div>
           <ModeToggle />
+
       </ThemeProvider>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {ClerkProvider} from "@clerk/react";
+import { shadcn } from '@clerk/ui/themes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <ClerkProvider
+          appearance={{
+              options: {
+                  socialButtonsPlacement: 'bottom',
+                  socialButtonsVariant: 'iconButton',
+                  termsPageUrl: 'https://clerk.com/terms',
+                  logoImageUrl: '/assets/vite.svg',
+                  logoLinkUrl: '/',
+                  logoPlacement: 'outside'
+              },
+              theme: shadcn,
+          }}
+      >
+          <App />
+      </ClerkProvider>
   </StrictMode>,
 )
