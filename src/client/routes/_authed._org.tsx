d9d809f2f81d0b46812924b6817502fbe9c21275
Charles Nkonoki <charlitokenn@gmail.com>
2026-08-10 21:14:38 +0300

    chore: removed unnecessary code

--- src/client/routes/_authed._org.tsx
+++ src/client/routes/_authed._org.tsx
@@ -38,15 +38,6 @@ const navItems = [
 
 function OrgLayout() {
   return (
-    // <div className="flex min-h-svh">
-    //   <SidebarNav className="hidden md:flex" />
-    //   <div className="flex min-w-0 flex-1 flex-col">
-    //     <TopBar />
-    //     <main className="flex-1 overflow-y-auto p-6">
-    //       <Outlet />
-    //     </main>
-    //   </div>
-    // </div>
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
