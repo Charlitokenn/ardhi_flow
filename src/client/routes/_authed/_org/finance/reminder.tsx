75c7c9bbe02ed7440d0a92a8b254c342c916f66a
Charles Nkonoki <charlitokenn@gmail.com>
2026-08-27 13:32:33 +0300

    updated stepper's animation

--- src/client/routes/_authed/_org/finance/reminder.tsx
+++ src/client/routes/_authed/_org/finance/reminder.tsx
@@ -1,7 +1,9 @@
 import {createFileRoute} from '@tanstack/react-router'
 import {PageHero} from "@/components/pageHero.tsx";
-import {BellRingIcon} from "lucide-react";
+import {BellRingIcon, MessagesSquareIcon} from "lucide-react";
 import {InstallmentsReminderDataGrid} from "@/components/data-grids/installments-reminder-datagrid.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {ReusableSheet} from "@/components-reusable/reusable-sheet.tsx"
 
 export const Route = createFileRoute('/_authed/_org/finance/reminder')({
     staticData: {
@@ -13,12 +15,24 @@ export const Route = createFileRoute('/_authed/_org/finance/reminder')({
 function RouteComponent() {
     return (
         <section className="-mt-4 -ml-1">
-            <PageHero
-                type="hero"
-                title="Reminder"
-                subtitle="Followup on outstanding payments"
-                buttonIcon={<BellRingIcon/>}
-            />
+            <div className="flex justify-between items-center">
+                <PageHero
+                    type="hero"
+                    title="Reminder"
+                    subtitle="Followup on outstanding payments"
+                    buttonIcon={<BellRingIcon/>}
+                />
+                <ReusableSheet
+                    title="Message Broadcasting"
+                    trigger={
+                        <Button variant="outline">
+                            <MessagesSquareIcon className="size-4"/> Message Broadcast
+                        </Button>
+                    }
+                    widthClassName="sm:max-w-full"
+                    children={<div/>}
+                />
+            </div>
             <InstallmentsReminderDataGrid/>
         </section>
     )
